f3e585d7dda5c79b414034a406ecfd40de5ad28f
vnmw7 (vincent) <146041946+vnmw7@users.noreply.github.com>
2025-03-23 15:48:26 +0800

    refactor: remove Sentry initialization and wrapping from RootLayout component

--- Mobile/app/_layout.tsx
+++ Mobile/app/_layout.tsx
@@ -1,27 +1,5 @@
 import { Stack } from "expo-router";
-import * as Sentry from "@sentry/react-native";
 
-Sentry.init({
-  dsn: "https://0b73a6ac26ee026a0548c72ce4ebe09b@o4509016564629504.ingest.us.sentry.io/4509021934321665",
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-
-  // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-  // We recommend adjusting this value in production.
-  tracesSampleRate: 1.0,
-  // profilesSampleRate is relative to tracesSampleRate.
-  // Here, we'll capture profiles for 100% of transactions.
-  profilesSampleRate: 1.0,
-
-  // While you're testing, we recommend that you set replaysSessionSampleRate to 1.0. This ensures that every user session will be sent to Sentry.
-  replaysSessionSampleRate: 1.0,
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [Sentry.mobileReplayIntegration()],
-});
-
-function RootLayout() {
+export default function RootLayout() {
   return <Stack />;
 }
-
-export default Sentry.wrap(RootLayout);
