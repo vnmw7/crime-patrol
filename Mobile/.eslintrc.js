7573e591511d24a93ce8e3510871d9989fbb184c
vnmw7 (vincent) <146041946+vnmw7@users.noreply.github.com>
2025-03-31 00:06:51 +0800

    fix: add jest setup file and mock necessary modules for testing

--- Mobile/.eslintrc.js
+++ Mobile/.eslintrc.js
@@ -1,5 +1,14 @@
 // https://docs.expo.dev/guides/using-eslint/
+// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ["expo", "prettier"],
   ignorePatterns: ["/dist/*"],
+  overrides: [
+    {
+      files: ["jest.setup.js"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
