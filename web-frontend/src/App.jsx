cfec04b28b9ed473b777d4ee6741a5b8bf7fca33
yahnnie <delossantosdianne032@gmail.com>
2025-06-03 19:59:15 +0800

    web-frontend routes

--- web-frontend/src/App.jsx
+++ web-frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import route pages
+import Dashboard from "./routes/Dashboard.jsx";
+import DashboardOverview from "./routes/dashboard.overview.jsx";
+import DashboardReports from "./routes/dashboard.reports.jsx";
+import DashboardSettings from "./routes/dashboard.settings.jsx";
+import DashboardChat from "./routes/dashboard.chats.jsx";
+import DashboardIncidents from "./routes/dashboard.incidents.jsx";
+import DashboardStations from "./routes/dashboard.stations.jsx";
+import Index from "./routes/index.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/overview" element={<DashboardOverview />} />
+        <Route path="/dashboard/reports" element={<DashboardReports />} />
+        <Route path="/dashboard/settings" element={<DashboardSettings />} />
+        <Route path="/dashboard/chat" element={<DashboardChat />} />
+        <Route path="/dashboard/incidents" element={<DashboardIncidents />} />
+        <Route path="/dashboard/stations" element={<DashboardStations />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
